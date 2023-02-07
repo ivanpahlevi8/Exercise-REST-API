69431b6f0b4e5348a13a6ae34023fffd0f722db1
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2023-02-07 22:28:34 +0700

    Updating itemservice with several function logic

--- pkg/service/itemservice.go
+++ pkg/service/itemservice.go
@@ -99,5 +99,225 @@ func (s *ItemService) GetitemByName(name string) (model.Item, error) {
 }
 
 func (s *ItemService) GetItemByPrice(price string) (model.Item, error) {
-	return model.Item{}, nil
+	var allItem []model.Item
+	var getItem model.Item
+	var err error
+
+	// make logci to find id based on price
+	var id string
+	allItem, err = s.ItemRepo.GetAllData()
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	}
+
+	check := false
+
+	for _, item := range allItem {
+		if price == item.GetItemPrice() {
+			check = true
+			id = item.GetItemId()
+			break
+		}
+	}
+
+	if check {
+		// if data exist
+		getItem, err = s.ItemRepo.GetData(id)
+		if err != nil {
+			log.Println(err)
+			return getItem, err
+		} else {
+			return getItem, nil
+		}
+	} else {
+		// if data not exist
+		errs := errors.New("get error when getting data with price")
+		return getItem, errs
+	}
+}
+
+func (s *ItemService) GetItemByDate(date string) (model.Item, error) {
+	// make variable
+	var allItem []model.Item
+	var err error
+	var getItem model.Item
+
+	// make logic to get id at certain date of data
+	var getId string
+
+	allItem, err = s.ItemRepo.GetAllData()
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	}
+
+	check := false
+
+	for _, item := range allItem {
+		if date == item.GetItemDate() {
+			check = true
+			getId = item.GetItemId()
+			break
+		}
+	}
+
+	if check {
+		// if data with certain date founded
+		getItem, err = s.ItemRepo.GetData(getId)
+
+		if err != nil {
+			log.Println(err)
+			return getItem, err
+		} else {
+			return getItem, nil
+		}
+	} else {
+		errs := errors.New("error happen when getting data using date")
+		return getItem, errs
+	}
+}
+
+func (s *ItemService) UpdateDataById(id string, inputItem model.Item) (model.Item, error) {
+	// make variable
+	var getItem model.Item
+	var err error
+
+	// do update
+	getItem, err = s.ItemRepo.UpdateData(id, inputItem)
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	} else {
+		return getItem, nil
+	}
+}
+
+func (s *ItemService) UpdateDataByName(name string, inputItem model.Item) (model.Item, error) {
+	// make variable
+	var allItem []model.Item
+	var getItem model.Item
+	var err error
+
+	// make logic to get data based on name
+	var getId string
+
+	allItem, err = s.ItemRepo.GetAllData()
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	}
+
+	check := false
+
+	for _, item := range allItem {
+		if name == item.GetItemName() {
+			check = true
+			getId = item.GetItemId()
+			break
+		}
+	}
+
+	if check {
+		// if item exist
+		getItem, err = s.ItemRepo.UpdateData(getId, inputItem)
+
+		if err != nil {
+			log.Println(err)
+			return getItem, err
+		} else {
+			return getItem, nil
+		}
+	} else {
+		errs := errors.New("error when updating data using item name")
+		return getItem, errs
+	}
+}
+
+func (s *ItemService) UpdateItemByPrice(price string, inputItem model.Item) (model.Item, error) {
+	// make variable
+	var allItem []model.Item
+	var getItem model.Item
+	var err error
+
+	// make logic to get item using price
+	var getId string
+
+	allItem, err = s.ItemRepo.GetAllData()
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	}
+
+	check := false
+
+	for _, item := range allItem {
+		if price == item.GetItemPrice() {
+			check = true
+			getId = item.GetItemId()
+			break
+		}
+	}
+
+	if check {
+		// if data founded
+		getItem, err = s.ItemRepo.UpdateData(getId, inputItem)
+
+		if err != nil {
+			log.Println(err)
+			return getItem, err
+		} else {
+			return getItem, nil
+		}
+	} else {
+		errs := errors.New("error when updating item using price")
+		return getItem, errs
+	}
+}
+
+func (s *ItemService) UpdateItemByDate(date string, item model.Item) (model.Item, error) {
+	// create variable
+	var allItem []model.Item
+	var getItem model.Item
+	var err error
+
+	// make logic to get item using date
+	var getId string
+
+	allItem, err = s.ItemRepo.GetAllData()
+
+	if err != nil {
+		log.Println(err)
+		return getItem, err
+	}
+
+	check := false
+
+	for _, item := range allItem {
+		if date == item.GetItemDate() {
+			check = true
+			getId = item.GetItemId()
+			break
+		}
+	}
+
+	if check {
+		// if data founded
+		getItem, err = s.ItemRepo.UpdateData(getId, item)
+
+		if err != nil {
+			log.Println(err)
+			return getItem, err
+		} else {
+			return getItem, nil
+		}
+	} else {
+		errs := errors.New("error when updating data using date")
+		return getItem, errs
+	}
 }
